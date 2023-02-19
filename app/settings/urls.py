2211db092dced39766f940f0e5f9d986a4eb7b5f
Natalie Borotkanych <nborotkanych@gmail.com>
2023-02-19 08:42:26 +0200

    Created contact form

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from main.views import hello_world
+from currency.views import list_rates, contact_form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', hello_world, )
+    path('hello-world/', hello_world),
+    path('rate/list', list_rates),
+    path('contactus', contact_form),
 ]
