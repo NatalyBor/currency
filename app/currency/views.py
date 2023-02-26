bfc18e8f478b02e6000f575058f613f578fab3f3
Natalie Borotkanych <nborotkanych@gmail.com>
2023-02-26 18:12:04 +0200

    errors checked hw7

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 from currency.models import Rate, ContactUs
 
@@ -8,24 +8,22 @@ def list_rates(request):
     rates = Rate.objects.all()
 
     context = {
-        'rates' : rates
+        'rates': rates
 
     }
 
     return render(request, 'rates_list.html', context)
-
-    # result = []
-    # for rate in qs:
-    #     result.append(f'id: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, currency: {rate.currency}, source: {rate.source}, created: {rate.created}<br>')
-    # return HttpResponse(str(result))
-
-
+# result = []
+# for rate in qs:
+#     result.append(f'id: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, currency: {rate.currency}, source: {rate.source}, created: {rate.created}<br>')
+# return HttpResponse(str(result))
 # def contact_form(request):
 #     qs = ContactUs.objects.all()
 #     result = []
 #     for client in qs:
 #         result.append(f'id: {client.id}, email_from: {client.email_from}, subject: {client.subject}, message: {client.message} <br>')
 #     return HttpResponse(str(result))
+
 
 def contact_form(request):
     contactus = ContactUs.objects.all()
