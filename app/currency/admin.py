cb33f8ebf1f728df72e12afb60fbfa294d47b588
Natalie Borotkanych <nborotkanych@gmail.com>
2023-03-11 17:36:20 +0200

    hw10 done

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,3 +1,67 @@
-# from django.contrib import admin
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from rangefilter.filter import DateRangeFilter
 
-# Register your models here.
+from currency.models import Rate, Source, ContactUs
+
+
+@admin.register(Rate)
+class RateAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sell',
+        'created',
+        'currency',
+        'source',
+    )
+    list_filter = (
+        'currency',
+        ('created', DateRangeFilter)
+    )
+    search_fields = (
+        'source',
+        'buy',
+        'sell',
+    )
+    # readonly_fields = (
+    #     'buy',
+    #     'sell',
+    # )
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+    #
+    # def has_add_permission(self, request):
+    #     return False
+    #
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+
+
+@admin.register(Source)
+class SourceAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'source_url',
+        'name'
+    )
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'email_from',
+        'subject',
+        'message',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- app/settings/settings.py
+++ app/settings/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'debug_toolbar',
+    'rangefilter',
+    'import_export',
 
     'main',
     'currency',
-
 ]
 
 MIDDLEWARE = [
