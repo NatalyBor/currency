e594bd43aa7ccb306155114428663084bc5c9a40
Natalie Borotkanych <nborotkanych@gmail.com>
2023-05-11 20:39:36 +0300

    hw17 flake8 errors done

--- app/currency/utils.py
+++ app/currency/utils.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 
-def to_2_places_decimal(value: str) -> Decimal:
+def to_2_point_decimal(value: str):
     '''Convert string to Decimal with 2 places
     example:
     '123.456' -> Decimal()'123.456'
